07ec1984d55c0e5836d5e334dd6e6cf1aae38938
faizvk <faizvk14@gmail.com>
2026-06-10 18:20:26 +0530

    Register planner models on metadata

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,9 @@
 from app.models.base import Base
 from app.models.file_asset import FileAsset
+from app.models.plan import Plan
+from app.models.plan_step import PlanStep
 from app.models.topic import Topic
 from app.models.user import User
 from app.models.version import Version
 
-__all__ = ["Base", "User", "Topic", "Version", "FileAsset"]
+__all__ = ["Base", "User", "Topic", "Version", "FileAsset", "Plan", "PlanStep"]
